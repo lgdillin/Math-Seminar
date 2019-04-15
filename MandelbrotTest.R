5c4e21feececafb70787b831422070d77fa98552
lgdillin <mailgd9@gmail.com>
2019-04-14 21:35:45 -0500

    did a bunch of work

--- MandelbrotTest.R
+++ MandelbrotTest.R
@@ -18,6 +18,15 @@ for(i in 1:length(init.values)) {
 }
 plot(init.values, growth, xlab = "Given y-intercept", ylab = "Orbit Value", main = "z = z^(2) + c", type="p", cex=.2)
 
+c = 0.2
+x=0
+for(i in 1:1000000) {
+  x = x^2 + c
+  if(i %% 1000 == 0) {
+    print(x) 
+  }
+}
+
 
 z = complex(real = 0, imaginary = 0)
 c = complex(real = 1, imaginary = -1)
@@ -66,21 +75,25 @@ in.mandelbrot.set <- function(c, iterations = 20, bound = 2) {
   for (i in 1:iterations) {
     z <- z ** 2 + c
     if (Mod(z) > bound) {
-      return(FALSE)
+      return(0)
     }
   }
-  return(TRUE)
+  return(1)
 }
 
 resolution <- 0.001
 sequence <- seq(-1, 1, by = resolution)
 m <- matrix(nrow = length(sequence), ncol = length(sequence))
+cc = rep(0, length(sequence)^2)
 for (x in sequence) {
   for (y in sequence) {
     mandelbrot <- in.mandelbrot.set(complex(real = x, imaginary = y))
-    m[round((x + resolution + 1) / resolution), round((y + resolution + 1) / resolution)] <- mandelbrot
+    #cc
+    #m[round((x + resolution + 1) / resolution), round((y + resolution + 1) / resolution)] <- mandelbrot
   }
 }
+plot(m)
+
 
 jpeg('mandelbrot.jpg')
 image(m)
